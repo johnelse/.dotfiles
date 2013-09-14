0d183e22a7766725f31ac354a2f729053102e8fe
John Else <john.else@citrix.com>
2013-09-14 11:03:12 +0100

    Add to aliases
    Signed-off-by: John Else <john.else@citrix.com>

--- bash/bash/aliases.sh
+++ bash/bash/aliases.sh
@@ -3,7 +3,52 @@ alias a="spotify-cli play-album"
 alias t="spotify-cli play-track"
 
 # Filesystem
-alias fn='find . -name'
 alias u='cd ..'
 alias uj='tar jxvf'
 # equivalent command uz is provided by Debian for gzip'd tarballs.
+
+# Searching
+alias fn='find . -name'
+alias na='grep -v autogen'
+
+# ocaml
+alias ocaml='rlwrap -cr ocaml'
+####################################################################################
+#
+# see: http://tech.groups.yahoo.com/group/ocaml_beginners/message/2929
+#
+# if you want a toplevel using thread :
+#   ocamlmktop -thread threads.cma -o ocamlthread
+#
+# You need then to launch it with :
+#
+#   ./ocamlthread -I +threads
+#
+# beware that if your ocaml is compiled with POSIX thread, you will need
+# to add unix.cma before threads.cma.
+#
+####################################################################################
+alias ocamlthread='rlwrap -cr ocamlthread -I +threads'
+
+# rdesktop
+alias adserver='rdesktop 10.80.237.222 -a 16 -k en-gb -K -x 80 -d TEST -u Administrator'
+alias adserver_small='adserver -g 1024x600'
+alias adserver_med='adserver -g 1366x768'
+alias adserver_large='adserver -g 1680x1050'
+
+alias adserver_2k3='rdesktop 10.80.237.223 -K -g 1440x900'
+
+# assorted shortcuts
+alias api="~/Code/utils/xenapi-helpers/run"
+alias erm='ssh ermintrude'
+alias irc='irssi -c FreeNode'
+
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias ls='ls -G --color=always'
+alias ll='ls -l'
+alias la='ls -a'
+alias less='less -R'
+alias gl='git log --graph --abbrev-commit --pretty=oneline --decorate'
+# "open <filename>" uses the default application for <filename>
+alias open='xdg-open'
