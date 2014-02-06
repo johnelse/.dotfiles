a595a0d30e8012040b8b2b61aea0a689617f0f99
John Else <john.else@citrix.com>
2014-02-06 15:52:37 +0000

    bash: Tidy up exports.sh
    Signed-off-by: John Else <john.else@citrix.com>

--- bash/bash/exports.sh
+++ bash/bash/exports.sh
@@ -4,28 +4,23 @@ export TERM=xterm-color
 # Locations
 export LOC=/local/johnel-files
 export ISSUES=$LOC/issues
-export CARBON1=$LOC/hg/carbon
-export CARBON2=/local/spare/hg/carbon
+export CARBON=/local/spare/hg/carbon
 
-export GEORGE_LCM=$CARBON2/george-lcm/build.hg
-export MNR_LCM=$CARBON2/mnr-lcm/build.hg
-export COWLEY_LCM=$CARBON1/cowley-lcm/build.hg
-export OXFORD_LCM=$CARBON1/oxford-lcm/build.hg
-export BOSTON_LCM=$CARBON2/boston-lcm/build.hg
-export SANIBEL_LCM=$CARBON2/sanibel-lcm/build.hg
-export TAMPA_LCM=$CARBON2/tampa-lcm/build.hg
-export CLEARWATER_LCM=$CARBON2/clearwater-lcm/build.hg
+export BOSTON_LCM=$CARBON/boston-lcm/build.hg
+export SANIBEL_LCM=$CARBON/sanibel-lcm/build.hg
+export TAMPA_LCM=$CARBON/tampa-lcm/build.hg
+export CLEARWATER_LCM=$CARBON/clearwater-lcm/build.hg
 
-export RING3=$CARBON2/trunk-ring3/build.hg
+export RING3=$CARBON/trunk-ring3/build.hg
 export RING3_CHROOT=$RING3/obj/chroot-dom0-env
 export RING3_API=$RING3/myrepos/xen-api
 export RING3_CODE=$RING3_API/ocaml
 
-export EA_1268=$CARBON2/trunk-ring3-ea1268/build.hg
-export PR_1061=$CARBON2/pr-1061/build.hg
-export PR_1675=$CARBON2/pr-1675/build.hg
+export EA_1268=$CARBON/trunk-ring3-ea1268/build.hg
+export PR_1061=$CARBON/pr-1061/build.hg
+export PR_1675=$CARBON/pr-1675/build.hg
 
-export PERF=$CARBON2/trunk-perf/build.hg
+export PERF=$CARBON/trunk-perf/build.hg
 
 # Editors
 export EDITOR=/usr/bin/vim
@@ -48,7 +43,7 @@ PS1="\[\e[0;32;49m\]\u@\h:\W\[\e[0;33;49m\]\$(parse_git_branch)\[\e[0;31;49m\]$\
 PATH=$PATH:/usr/groups/xencore/systems/bin
 PATH=$PATH:/usr/groups/xenrt/production/share/control
 PATH=$PATH:~/Scripts/johnel
-PATH=$PATH:$CARBON2/trunk-ring3/support.hg/bugget
+PATH=$PATH:$CARBON/trunk-ring3/support.hg/bugget
 PATH=$PATH:$LOC/bin
 PATH=$PATH:~/bin
 PATH=$PATH:/usr/local/installed/opam/bin
