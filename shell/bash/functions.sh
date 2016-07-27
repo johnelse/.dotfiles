ea7ee0ed93f1e254ad164d4550d605c708e20bbe
John Else <john.else@gmail.com>
2016-07-27 11:25:41 +0100

    shell: remove some functions
    Signed-off-by: John Else <john.else@gmail.com>

--- shell/bash/functions.sh
+++ shell/bash/functions.sh
@@ -17,50 +17,6 @@ function cdi() {
 }
 
 # Laziness.
-function ca() {
-  cd $ISSUES/bug-CA-$@
-}
-
-function nvidia() {
-  cd $ISSUES/bug-NVIDIA-$@
-}
-
-function sctx() {
-  cd $ISSUES/bug-SCTX-$@
-}
-
-function xop() {
-  cd $ISSUES/bug-XOP-$@
-}
-
-# Download all files for the given ticket.
-function get_ticket() {
-  TICKET_TYPE=$1
-  TICKET_NUMBER=$2
-  cd $ISSUES
-  bugget.py ${TICKET_TYPE}-${TICKET_NUMBER} && ${TICKET_TYPE,,} ${TICKET_NUMBER}
-}
-
-function ca_ticket() {
-  get_ticket CA $1
-}
-
-function nvidia_ticket() {
-  get_ticket NVIDIA $1
-}
-
-function sctx_ticket() {
-  get_ticket SCTX $1
-}
-
-function xop_ticket() {
-  get_ticket XOP $1
-}
-
-function code() {
-  cd /usr/groups/sources/git/carbon/$@
-}
-
 function ctime() {
   python -c "import time; print time.ctime(${1})"
 }
@@ -86,13 +42,6 @@ function cifs_mount() {
 function my_cifs_mount() {
   MOUNTPOINT=$1
   cifs_mount //lonpfs01/johnel$ $MOUNTPOINT
-}
-
-function lock_reason() {
-  HOST=$1
-  REASON=$2
-  xenuse -r $HOST
-  xenuse -l $HOST $REASON
 }
 
 function get_logs() {
