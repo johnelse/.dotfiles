9bde03ea50addf24288a5b630f45f39e70c5d525
John Else <john.else@gmail.com>
2022-01-02 19:29:48 +0000

    shell: Add function tidy_docker

--- shell/bash/functions.sh
+++ shell/bash/functions.sh
@@ -46,3 +46,8 @@ function mergeto {
 function acvs() {
   cd ~/issues/ACVS-${1}
 }
+
+function tidy_docker() {
+    docker ps -a | grep "Exited " | awk '{print $1}' | xargs docker rm
+    docker images | grep "\<none\>" | awk '{print $3}' | xargs docker rmi
+}
