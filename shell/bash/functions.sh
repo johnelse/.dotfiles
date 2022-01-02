8aee6dc0367793bbc15f968c20e239296bb621ff
John Else <john.else@gmail.com>
2022-01-02 19:26:11 +0000

    shell: Tidy functions.sh

--- shell/bash/functions.sh
+++ shell/bash/functions.sh
@@ -33,27 +33,6 @@ function md5each() {
   done
 }
 
-# Mount windows shares.
-# CIFS path needs to be single quoted, e.g.
-# cifs_mount '\\host.citrite.net\foo\bar\' /mnt
-function cifs_mount() {
-  SHARE=$(echo $1 | sed -e 's/\\/\//g')
-  MOUNTPOINT=$2
-  # Unmount if anything is already mounted.
-  mountpoint -q $MOUNTPOINT && sudo umount $MOUNTPOINT
-  sudo mount.cifs $SHARE $MOUNTPOINT -o user=johnel dom=citrite.net
-}
-
-function my_cifs_mount() {
-  MOUNTPOINT=$1
-  cifs_mount //lonpfs01/johnel$ $MOUNTPOINT
-}
-
-function to_vm() {
-  FILE=$1
-  scp $FILE john@192.168.56.101:/tmp
-}
-
 # Useful when tmux isn't in a common $PATH
 function sshtmux() {
     ssh $1 -t 'exec bash -l -c "tmux attach"'
